8d24a9e2c130adbe9a0cdd2413eea702299884fb
it_st it <overult01@gmail.com>
2022-05-19 16:32:21 +0900

    @Before: 각 JUnit 테스트 진행시 필요한 공통적인 초기화 담당 (가독성 향상)

--- iloveyouboss_06/test/iloveyouboss/ProfileTest.java
+++ iloveyouboss_06/test/iloveyouboss/ProfileTest.java
@@ -8,19 +8,33 @@
 ***/
 package iloveyouboss;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.*;
 
 public class ProfileTest {
+	
+	private Profile profile;
+	private BooleanQuestion question;
+	private Criteria criteria;
+	
+	// @Before: 각 JUnit 테스트 진행시 필요한 공통적인 초기화 담당 (가독성 향상)
+	@Before
+	public void create() { // profile, question, criteria 변수를 초기화 
+		profile = new Profile("Bull Hockey, Inc.");
+	    question = new BooleanQuestion(1, "Got bonuses?");
+	    criteria = new Criteria();
+	}	
+	
 
-// 준비-실행-단언(AAA) 중 1) 테스트 준비 단계(Arrange)
    @Test
    public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
 
-      Profile profile = new Profile("Bull Hockey, Inc.");
-      Question question = new BooleanQuestion(1, "Got bonuses?");
-      
+	   // 준비-실행-단언(AAA) 중 1) 테스트 준비 단계(Arrange)
+	   
+	   // @Before가 대신해주는 덕분에 초기화 부분 생략 
+	   
       // 질문에 대한 실제 대답 
       Answer profileAnswer = new Answer(question, Bool.FALSE);
       profile.add(profileAnswer); 
@@ -31,21 +45,28 @@ public class ProfileTest {
       // 실제 대답에 대한 가중치 저장 
       Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
       
-      Criteria criteria = new Criteria();
       criteria.add(criterion);
+      
+      // 준비-실행-단언(AAA) 중 2) 테스트 실행 단계(Action)
+      boolean matches = profile.matches(criteria);
+      
+      // 준비-실행-단언(AAA) 중 3) 테스트 단언 단계(Assert) 
+      // 실패한 단언문 아래의 코드는 수행x. 따라서 단언문은 ㅌ스트의 가장 아래에 넣는다.
+      assertFalse(matches);
    }
    
+   // 또 다른 테스트이기 때문에 JUnit은 ProfileTest 인스턴스를 새롭게 생성 
    @Test
    public void matchAnswersTrueForAnyDontCareCriteria() {
-      Profile profile = new Profile("Bull Hockey, Inc.");
-      Question question = new BooleanQuestion(1, "Got milk?");
+
+	   // @Before가 대신해주는 덕분에 초기화 부분 생략 
+
       Answer profileAnswer = new Answer(question, Bool.FALSE);
       profile.add(profileAnswer);      
       Answer criteriaAnswer = new Answer(question, Bool.TRUE);
       
       // 실제 대답에 대한 가중치 저장
       Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
-      Criteria criteria = new Criteria();
       criteria.add(criterion);
 
       // 1번 테스트에서 추가된 2줄 

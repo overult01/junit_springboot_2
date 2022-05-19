5236801ff56d10c276930361355c03919e9eb28b
it_st it <overult01@gmail.com>
2022-05-19 15:15:46 +0900

    assertSame 으로 기대값과 같게 나오는지 테스트

--- iloveyouboss_01/test/iloveyouboss/ScoreCollectionTest.java
+++ iloveyouboss_01/test/iloveyouboss/ScoreCollectionTest.java
@@ -1,14 +1,25 @@
 package iloveyouboss;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertSame;
 
 import org.junit.Test;
 
 public class ScoreCollectionTest {
 
 	@Test
-	public void temp() {
-		fail("Not yet implemented");
-	}
+	public void answerArithmeticMeanOfTwoNumbers() {
+		
+		// 준비 
+		ScoreCollection collection = new ScoreCollection();
+		collection.add(()->5);
+		collection.add(()->7);
 
+		// 실행
+		int actualResult = collection.arithmeticMean();
+		
+		// 단언 (Deprecated된 assertThat메서드 대체)
+		assertSame(actualResult, 6); // assertSame(Object expected, Object actual)
+ 
+		}	
 }

270ae2083f7164d71cb456c8a1bdc97f5c273608
it_st it <overult01@gmail.com>
2022-05-19 15:57:38 +0900

    준비-실행-단언(AAA) 중 1) 테스트 준비 단계(Arrange)

--- iloveyouboss_06/test/iloveyouboss/ProfileTest.java
+++ iloveyouboss_06/test/iloveyouboss/ProfileTest.java
@@ -12,13 +12,24 @@ import org.junit.*;
 
 public class ProfileTest {
 
+// 준비-실행-단언(AAA) 중 1) 테스트 준비 단계(Arrange)
    @Test
-   public void test() {
+   public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
+
       Profile profile = new Profile("Bull Hockey, Inc.");
       Question question = new BooleanQuestion(1, "Got bonuses?");
-      Criteria criteria = new Criteria();
+      
+      // 질문에 대한 실제 대답 
+      Answer profileAnswer = new Answer(question, Bool.FALSE);
+      profile.add(profileAnswer); 
+      
+      // 질문에 대한 기대하는 대답  
       Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+      
+      // 실제 대답에 대한 가중치 저장 
       Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
+      
+      Criteria criteria = new Criteria();
       criteria.add(criterion);
    }
 }
